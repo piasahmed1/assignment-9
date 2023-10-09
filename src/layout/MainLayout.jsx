da6e89d30fab89981728f62bd42d9154dc42ce68
piasahmed1 <ahmedpias229@gmail.com>
2023-10-09 19:44:10 +0600

    This Project fully completed with responsive and all

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -4,7 +4,7 @@ import Navbar from '../Header/Navbar';
 const MainLayout = () => {
     return (
         <div>
-           <Navbar></Navbar>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,28 +5,26 @@ import Footer from "../../component/footer/Footer";
 import Banner2 from "../../component/footer/banner2/Banner2";
 import BCard from "../../component/bCard/BCard";
 
-
-
 const Home = () => {
 
   const cards = useLoaderData();
-  
-    return (
-        <div>
-         
-        <div className="w-[360px] md:w-[1000px] lg:w-[1220px] mx-auto">
+
+  return (
+    <div>
+
+      <div className="w-[360px] md:w-[1000px] lg:w-[1220px] mx-auto">
         <Banner></Banner>
-        </div>
-          <Service cards = {cards}></Service>
-        <div className="w-[360px] md:w-[1000px] lg:w-[1220px] mx-auto">
+      </div>
+      <Service cards={cards}></Service>
+      <div className="w-[360px] md:w-[1000px] lg:w-[1220px] mx-auto">
         <Banner2></Banner2>
-        </div>
-         <div>
-         <BCard></BCard>
-         </div>
-          <Footer></Footer>
-        </div>
-    );
+      </div>
+      <div>
+        <BCard></BCard>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
